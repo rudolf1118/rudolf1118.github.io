5b142fcd84b1890cf30f3d1d6f1ce2a906e3a991
rudolf1118 <rud.haroutyunyan@gmail.com>
2022-05-22 21:28:51 +0400

    TImer

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import About from "./components/About";
 import {Route, Routes,Switch } from "react-router-dom";
 import CV from "./components/CV";
 import About1 from "./components/About1";
+import Timer from "./Timer";
 
 const App=()=>{
     return (
@@ -14,6 +15,7 @@ const App=()=>{
                         <Route path={"/home"} element={<About/>}/>
                         <Route path={"/CV"} element={<CV/>}/>
                 </Routes>
+            <Timer/>
         </div>
 
     );
--- src/components/CVs.jsx
+++ src/components/CVs.jsx
@@ -1,18 +1,15 @@
-import react from "react";
-import {IoTerminal} from "react-icons/io5";
 import cv from './cv.png'
 import classes from "./Links.module.css";
-import {NavLink} from "react-router-dom";
+
+
 let dateObj=new Date();
 let month=dateObj.getUTCMonth() + 1
 let year=dateObj.getUTCFullYear();
 let day=dateObj.getDate()
 console.log(month, year)
-
 let monther=4;
 let yearer=2022;
 let dayer=1;
-
 let deltaMont=month - monther;
 let deltaYeat=year - yearer
 let deltaDay=day - dayer;
@@ -29,8 +26,8 @@ const CVs=()=>{
             <img src={cv} className={classes.port}/>
             <div>
                 <a href={"https://www.linkedin.com/company/teams-am/"} target={"blank_"}>
-                <img className={classes.company}
-                     src={'https://media-exp1.licdn.com/dms/image/C4D0BAQFYNTW8txC5GA/company-logo_200_200/0/1614712703866?e=1661385600&v=beta&t=f_1AaEwkoLRVmw0fcx8H3bKP0_TJzSTbCf8ovvVXWJ4'}/>
+                    <img className={classes.company}
+                         src={'https://media-exp1.licdn.com/dms/image/C4D0BAQFYNTW8txC5GA/company-logo_200_200/0/1614712703866?e=1661385600&v=beta&t=f_1AaEwkoLRVmw0fcx8H3bKP0_TJzSTbCf8ovvVXWJ4'}/>
                 </a>
                 <div className={classes.cvAttr}> Teams.am</div>
                 <div className={classes.hastiq}>Front-End Developer</div>
