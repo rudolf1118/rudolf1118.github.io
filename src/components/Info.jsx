d3889ccac283692920cee577a96f9f599ed9dbaf
rudolf1118 <rud.haroutyunyan@gmail.com>
2022-05-22 22:02:04 +0400

    new bg

--- src/components/Info.jsx
+++ src/components/Info.jsx
@@ -1,12 +1,13 @@
 import react from 'react'
 import classes from "./Links.module.css";
+import back from "./back.jpg"
 
 const Info=()=>{
     return (
         <>
             <div className={classes.photo}>
                 <img
-                    className={classes.photo} src={'https://static.wixstatic.com/media/c7e00e0037b14973aa4cca10d675a3ec.jpg/v1/fill/w_1902,h_880,fp_0.50_0.50,q_85,usm_0.66_1.00_0.01,enc_auto/c7e00e0037b14973aa4cca10d675a3ec.jpg'}/>
+                    className={classes.photo} src={back}/>
             </div>
             <div className={classes.text}>
                 RUDOLF HARUTYUNYAN <br/>WELCOMES YOU <br/>
